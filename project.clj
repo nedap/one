d68c3e8603ab3eee0338c8594d718ec5465aac4e
vemv <vemv@users.noreply.github.com>
2019-06-26 14:28:55 +0200

    v1.0.0 (#1)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.nedap.staffing-solutions/one "0.1.0-alpha1"
+(defproject com.nedap.staffing-solutions/one "1.0.0"
   ;; Please keep the dependencies sorted a-z.
   :dependencies [[com.nedap.staffing-solutions/utils.modular "0.3.0"]
                  [com.nedap.staffing-solutions/utils.spec "0.8.2"
